fd9fed7ada59e53d6ab0781f6d552a539748f68a
Santiago Rey <dsrey9435@gmail.com>
2023-04-30 22:47:53 -0500

    Configuración de boton que activa la alerta en el dispositivo

---
+++ app/src/main/java/com/sr/epilepsyalarm/infrastructure/ButtonService.kt
@@ -0,0 +1,63 @@
+package com.sr.epilepsyalarm.infrastructure
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.sr.epilepsyalarm.R
+import com.sr.configuration.util.Constants.channelId
+import com.sr.configuration.util.Constants.channelName
+
+class ButtonService : Service() {
+
+
+    private val lockedReceiver= LockedReceiver()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        val filter = IntentFilter(Intent.ACTION_SCREEN_ON)
+        filter.priority = IntentFilter.SYSTEM_HIGH_PRIORITY - 1
+
+
+        registerReceiver(lockedReceiver, filter)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(lockedReceiver)
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(getString(R.string.app_name))
+            .setContentText("Tu mensaje aquí")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        val channel = NotificationChannel(
+            channelId,
+            channelName,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+
+        startForeground(1, notificationBuilder.build())
+
+
+        return START_STICKY
+    }
+
+
+}
