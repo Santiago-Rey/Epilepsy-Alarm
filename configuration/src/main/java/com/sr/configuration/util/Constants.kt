81d62f20a6f81bc199baffccc88f4aa7105941c7
Santiago Rey <dsrey9435@gmail.com>
2023-05-01 18:24:45 -0500

    Objeto dond ese crean constantes que se usan en varias partes del codigo

---
+++ configuration/src/main/java/com/sr/configuration/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.sr.configuration.util
+
+object Constants {
+
+    val channelId = "alert_notification"
+    val channelName = "Alert Notification"
+    val channelDescription =
+        "Canal de notificación para mostrar mensajes en la pantalla de bloqueo"
+    val keySound = "key_sound"
+    val sharedPreferenceName = "epilepsy_pref"
+}
