4165deade890b97e126c41e7ef914d7832335917
Santiago Rey <dsrey9435@gmail.com>
2023-06-21 03:51:01 -0500

    - Se agrega metodo para el envio del mensaje por medio de whatsapp

--- app/src/main/java/com/sr/epilepsyalarm/data/repository/MessageRepository.kt
+++ app/src/main/java/com/sr/epilepsyalarm/data/repository/MessageRepository.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.telephony.SmsManager
 import com.sr.configuration.data.SharedPrefsRepositoryImpl
 import com.sr.configuration.domain.UserUseCase
-import com.sr.epilepsyalarm.data.MessageApi
-import com.sr.epilepsyalarm.data.MessageEntity
 import com.sr.configuration.data.SharedPreferenceDataSourceImpl
-import com.sr.epilepsyalarm.data.Text
 import com.sr.configuration.domain.PreferenceUseCase
 import com.sr.configuration.domain.UserModel
+import com.sr.epilepsyalarm.data.*
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -27,6 +25,18 @@ class MessageRepository {
         )
     }
 
+    suspend fun sendInitMessage() {
+        getRetrofit().create(MessageApi::class.java).postMessage(
+            MessageEntity("whatsapp",
+                null,
+                "57${getPhone()}",
+                "template",
+                null,
+                Template("hello_world", Language("en_US"))
+            )
+        )
+    }
+
 
     suspend fun sendSMS(context: Context) {
         val smsManager = SmsManager.getDefault()
@@ -40,7 +50,7 @@ class MessageRepository {
                 val original = chain.request()
 
                 val request = original.newBuilder()
-                    .header("Authorization", "Bearer EAAKt4SA1Vy0BADCZAyefz3rtnsopgQnov3m67qekt1W3h7UHdpSdEdZASpujJkXSiTaPev18ls6MZBbwXclq9u1kJO2sOPzTIDXZCJEVQZBlbZCT9JLyL2ZCBIHQv5abZC2S6W2NUORrvLoY7UFiBPDIcSMsssVl8zl9YHpZAN7o6qoq9Tkg9LmgZAZBGnGhyXXCx2PGOh8nVrtDoGKk0ZCYNqBbJPC5nX9k2aIZD")
+                    .header("Authorization", "Bearer EAAKt4SA1Vy0BAAZCbxpLP2mGujGmZAVPvbogXI5NPFDhpQZAX723O0uZBmXrZAqtAwybZCxlTZBYvKCJMMcaPOflUQQ7ipVpOae9EpiVPL992Cw6CxrDPtJ2nZAfMH4V52lwcFBraWBS49akVKa8ZAyZC2ZByLyDw3k9WyXfWvn87VL4zSlPr7ZAyJUe")
                     .method(original.method(), original.body())
                     .build()
 
